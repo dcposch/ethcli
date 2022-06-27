f8766961909ef641e6ee7aba9f441640098e3e93
DC <dcposch@dcpos.ch>
2022-06-27 04:31:49 -0400

    forge init

---
+++ packages/unicli/script/UniswapFrontend.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "forge-std/Script.sol";
+
+contract ContractScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+        new UniswapFrontend();
+    }
+}
